0acfc09744dfe02519e851f6b527c47277bbc581
shubham-aneja <shubham.aneja@snapdeal.com>
2020-08-18 20:59:24 +0530

    initial commit deploy

---
+++ precache-manifest.7bb3116a59b9b8bc2ce00f650d320cae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc975f06663b7e24a8432bc55c563d18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "436605149aa2597791eb",
+    "url": "/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "f7613a8700059cf1cf39",
+    "url": "/static/css/main.87f61a80.chunk.css"
+  },
+  {
+    "revision": "436605149aa2597791eb",
+    "url": "/static/js/2.b103b96a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b103b96a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7613a8700059cf1cf39",
+    "url": "/static/js/main.55416e15.chunk.js"
+  },
+  {
+    "revision": "c37a51658edd97cfa8b4",
+    "url": "/static/js/runtime-main.a2e684c6.js"
+  }
+]);
